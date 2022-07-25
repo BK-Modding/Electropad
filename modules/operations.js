adcec29ecb25f5ec55b8cb02a98be45485fa28e7
BK-Modding <kavesbteja@gmail.com>
2022-07-25 23:03:36 +0530

    Continuing to refactor code using Promises

--- modules/operations.js
+++ modules/operations.js
@@ -13,18 +13,22 @@ const nameFromPath = (filepath) => {
 }
 
 const saveFile = (currentWindow, savedPath) => {
-    currentWindow.webContents.executeJavaScript('document.getElementById("editor").value', true).then(textData => {
-        fs.writeFile(savedPath, textData, (err) => {
-            if (err) throw err;
+    return currentWindow.webContents.executeJavaScript('document.getElementById("editor").value', true).then(textData => {
+        return new Promise((resolve, reject) => {
+            fs.writeFile(savedPath, textData, (err) => {
+                if (err) reject(err);
 
-            console.log("Saved!");
-            currentWindow.webContents.send("file:saved", savedPath);
+                console.log("Saved!");
+                currentWindow.webContents.send("file:saved", savedPath);
+
+                resolve("Saved!");
+            });
         });
     });
 }
 
 const saveAsDialog = (currentWindow, previouslySavedPath) => {
-    dialog.showSaveDialog(currentWindow, {
+    return dialog.showSaveDialog(currentWindow, {
         title: 'Save As',
         defaultPath: previouslySavedPath ?? path.resolve(process.env.HOMEPATH, '*.txt'),
         buttonLabel: 'Save',
@@ -34,7 +38,7 @@ const saveAsDialog = (currentWindow, previouslySavedPath) => {
         ]
     }).then(file => {
         if (!file.canceled) {
-            saveFile(currentWindow, file.filePath.toString());
+            return saveFile(currentWindow, file.filePath.toString());
         }
     }).catch(err => {
         console.log(err);
@@ -42,30 +46,34 @@ const saveAsDialog = (currentWindow, previouslySavedPath) => {
 }
 
 const saveFileHandler = (currentWindow, saveAsFlag) => {
-    currentWindow.webContents.executeJavaScript('sessionStorage.getItem("filepath")', true).then(filepath => {
+    return currentWindow.webContents.executeJavaScript('sessionStorage.getItem("filepath")', true).then(filepath => {
         if (filepath) { // file loaded
             if (saveAsFlag) {
-                saveAsDialog(currentWindow, filepath);
+                return saveAsDialog(currentWindow, filepath);
             } else {
-                saveFile(currentWindow, filepath);
+                return saveFile(currentWindow, filepath);
             }
         } else {
-            saveAsDialog(currentWindow);
+            return saveAsDialog(currentWindow);
         }
     });
 }
 
 const openFile = (currentWindow, openPath) => {
-    fs.readFile(openPath, 'utf8', (err, content) => {
-        if (err) throw err;
+    return new Promise((resolve, reject) => {
+        fs.readFile(openPath, 'utf8', (err, content) => {
+            if (err) reject(err);
 
-        console.log("Opened!");
-        currentWindow.webContents.send('file:open', openPath, content);
+            console.log("Opened!");
+            currentWindow.webContents.send('file:open', openPath, content);
+
+            resolve("Opened!");
+        });
     });
 }
 
 const openFileDialog = (currentWindow, previouslySavedPath) => {
-    dialog.showOpenDialog(currentWindow, {
+    return dialog.showOpenDialog(currentWindow, {
         title: 'Open',
         defaultPath: previouslySavedPath ?? path.resolve(process.env.HOMEPATH, '*.txt'),
         buttonLabel: 'Open',
@@ -76,7 +84,7 @@ const openFileDialog = (currentWindow, previouslySavedPath) => {
         properties: ['openFile']
     }).then(files => {
         if (!files.canceled) {
-            openFile(currentWindow, files.filePaths[0]);
+            return openFile(currentWindow, files.filePaths[0]);
         }
     }).catch(err => {
         console.log(err);
@@ -84,11 +92,43 @@ const openFileDialog = (currentWindow, previouslySavedPath) => {
 }
 
 const openFileHandler = (currentWindow) => {
-    currentWindow.webContents.executeJavaScript('sessionStorage.getItem("filepath")', true).then(filepath => {
+    return currentWindow.webContents.executeJavaScript('sessionStorage.getItem("filepath")', true).then(filepath => {
         if (filepath) {
-            openFileDialog(currentWindow, filepath)
+            return openFileDialog(currentWindow, filepath)
         } else {
-            openFileDialog(currentWindow);
+            return openFileDialog(currentWindow);
+        }
+    });
+}
+
+const saveChangesPrompt = (currentWindow, callback) => {
+    return currentWindow.webContents.executeJavaScript('sessionStorage.getItem("modified")', true).then(modified => {
+        if (modified) {
+            return currentWindow.webContents.executeJavaScript('sessionStorage.getItem("filepath")', true).then(filepath => {
+                let title = filepath ?? 'Untitled';
+
+                return dialog.showMessageBox(newWindow, {
+                    type: 'none',
+                    buttons: ['Save', "Don't Save", 'Cancel'],
+                    defaultId: 0,
+                    title: 'Notepad',
+                    message: `Do you want to save changes to ${title}?`,
+                    cancelId: 2,
+                    noLink: true
+                }).then(result => {
+                    if (result.response === 0) {
+                        return saveFileHandler(newWindow, false).then(() => {
+                            callback();
+                        });
+                    } else if (result.response === 1) {
+                        callback();
+                    }
+                }).catch(err => {
+                    console.log(err);
+                });
+            });
+        } else {
+            callback();
         }
     });
 }
@@ -96,5 +136,6 @@ const openFileHandler = (currentWindow) => {
 module.exports = {
     saveFileHandler,
     openFileHandler,
-    nameFromPath
+    nameFromPath,
+    saveChangesPrompt
 }
